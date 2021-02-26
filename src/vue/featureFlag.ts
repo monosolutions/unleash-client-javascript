3ef05a63495640e537574bf410aced3f79a6ca01
Magnus Udbjørg <mlu@monosolutions.com>
2021-02-26 14:49:05 +0100

    making a vue directive

---
+++ src/vue/featureFlag.ts
@@ -0,0 +1,29 @@
+import { Unleash } from '../index';
+import { Context } from '../context';
+const unleash = new Unleash({
+  appName: 'async-unleash',
+  instanceId: 'browsers',
+  refreshInterval: 2400000,
+  url: '/api/feature',
+});
+unleash.on('error', console.error);
+
+unleash.on('ready', () => {
+  console.log('Features are ready');
+});
+export default (context: Context) => {
+  return {
+    install(Vue: any) {
+      /* istanbul ignore next */
+      Vue.directive('featureFlag', (el: Element, binding: { value: string; arg: any }) => {
+        const flag = binding.value;
+        const hideClass = binding.arg === 'visibility' ? 'userlimitHiddenV' : 'userlimitHidden';
+        if (unleash.isEnabled(flag, context)) {
+          el.classList.remove(hideClass);
+        } else {
+          el.classList.add(hideClass);
+        }
+      });
+    },
+  };
+};
